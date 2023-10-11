e027f1307c67d258e8bbea3211f5de1e1b300153
새미 <s22058@gsm.hs.kr>
2023-10-11 17:58:52 +0900

    Initial commit

---
+++ LoLViz/Helper/UIColor+Extension.swift
@@ -0,0 +1,17 @@
+//
+//  UIColor+Extension.swift
+//  LoLViz
+//
+//  Created by 새미 on 2023/10/11.
+//
+
+import UIKit
+
+extension UIColor {
+    @nonobjc class var background: UIColor { return UIColor(named: "background")! }
+    @nonobjc class var darkGreen: UIColor { return UIColor(named: "darkGreen")! }
+    @nonobjc class var neonGreen: UIColor { return UIColor(named: "neonGreen")! }
+    @nonobjc class var pink: UIColor { return UIColor(named: "pink")! }
+    @nonobjc class var blue: UIColor { return UIColor(named: "blue")! }
+    @nonobjc class var purple: UIColor { return UIColor(named: "purple")! }
+}
